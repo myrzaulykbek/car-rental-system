e5fd9d4c8cc06d95fac5036b79c72bf22190207a
Yernar <t.yernar.n@gmail.com>
2025-04-06 16:06:47 +0600

    Это значит, что теперь у всех машин в базе есть значения vin_number, и всё отображается правильно

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import CarViewSet, home
 
 router = DefaultRouter()
-router.register(r'cars', CarViewSet)
+router.register(r'cars', CarViewSet, basename="car")
 
 urlpatterns = [
     path('', home, name='home'),
--- main/views.py
+++ main/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticated
 from .models import Car
 from .serializers import CarSerializer
-from django_filters.rest_framework import DjangoFilterBackend
-from .filters import CarFilter
 
 def home(request):
     return HttpResponse("Добро пожаловать в систему аренды автомобилей!")
 
 class CarViewSet(viewsets.ModelViewSet):
-    queryset = Car.objects.all()
+    queryset = Car.objects.filter(is_available=True)  # Фильтр: только доступные авто
     serializer_class = CarSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_class = CarFilter
+    permission_classes = [IsAuthenticated]  # Доступ только для авторизованных пользователей
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]  # Фильтрация и поиск
+    ordering_fields = ["price_per_day", "year"]  # Сортировка по цене или году выпуска
+    search_fields = ["brand", "model"]  # Поиск по марке и модели
+
