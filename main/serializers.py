b057c540b6206146d574514a040bb86a22f43967
Admin <mtagabay22@gmail.com>
2025-04-13 15:16:46 +0500

    внедрил проверку доступности автомобиля перед бронированием

--- main/serializers.py
+++ main/serializers.py
@@ -3,6 +3,16 @@ from rest_framework import serializers
 from .models import Car, Rental
 from django.contrib.auth.models import User
 
+def is_car_available(car, start_date, end_date):
+    overlapping_rentals = Rental.objects.filter(
+        car=car,
+        status__in=['pending', 'active'],
+        start_date__lt=end_date,
+        end_date__gt=start_date
+    )
+    return not overlapping_rentals.exists()
+
+
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
@@ -13,7 +23,14 @@ class RentalSerializer(serializers.ModelSerializer):
         model = Rental
         fields = ['id', 'user', 'car', 'start_date', 'end_date', 'status']
 
+    def validate(self, data):
+        car = data['car']
+        start_date = data['start_date']
+        end_date = data['end_date']
 
+        if not is_car_available(car, start_date, end_date):
+            raise serializers.ValidationError("Этот автомобиль уже забронирован на выбранные даты.")
+        return data
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
